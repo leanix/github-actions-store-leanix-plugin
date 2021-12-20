48e2d00e48a15bbd8735ebc5154bce0e5fc02457
aravindmetku <aravind.metku@leanix.net>
2021-12-20 18:47:22 +0530

    cid-477 format bash file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,21 @@
 
 set -e
 
-if [[ $1 = "MAVEN" ]]; then
-    echo "Maven repository detected. Attempting to generate dependency file"
-    mvn license:download-licenses
+if [[ $1 == "MAVEN" ]]; then
+  echo "Maven repository detected. Attempting to generate dependency file"
+  mvn license:download-licenses
 fi
 
-if [[ $1 = "NPM" ]]; then
-    echo "Node repository detected. Attempting to generate dependency file"
-    npm i -g license-checker
-    npm i
-    license-checker --json > licenses.json
+if [[ $1 == "NPM" ]]; then
+  echo "Node repository detected. Attempting to generate dependency file"
+  npm i -g license-checker
+  npm i
+  license-checker --json >licenses.json
 fi
 
-if [[ $1 = "GRADLE" ]]; then
-    echo "Gradle repository detected. Attempting to generate dependency file"
-    gradle generateLicenseReport -I /miCiCd-init.gradle
+if [[ $1 == "GRADLE" ]]; then
+  echo "Gradle repository detected. Attempting to generate dependency file"
+  gradle generateLicenseReport -I /miCiCd-init.gradle
 fi
 
 node /dist/index.js
